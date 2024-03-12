6d8789ee7d3272a1afd13184a00c881737e6bb57
Kevin Plunkett <kaplunkett@gmail.com>
2024-03-12 15:29:24 -0400

    added slug field

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.text import slugify
 
 class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     body = RichTextUploadingField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
