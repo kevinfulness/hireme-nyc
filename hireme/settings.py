1348f755a8252bf4b61b34246688e4ed081425cf
Kevin Plunkett <kaplunkett@gmail.com>
2024-03-20 12:53:46 -0400

    changed db settings

--- hireme/settings.py
+++ hireme/settings.py
@@ -86,8 +86,9 @@ WSGI_APPLICATION = 'hireme.wsgi.application'
 # }
 # DATABASE_URL = os.environ.get('DATABASE_URL')
 # db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
 
 #if 'ENGINE' in DATABASES['default'] and 'postgres' in DATABASES['default']['ENGINE']:
 #    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
