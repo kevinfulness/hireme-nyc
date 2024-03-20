5d35af708f427e17550883a815ccc467198e30bc
Kevin Plunkett <kaplunkett@gmail.com>
2024-03-20 12:02:31 -0400

    changed db settings

--- hireme/settings.py
+++ hireme/settings.py
@@ -84,14 +84,13 @@ WSGI_APPLICATION = 'hireme.wsgi.application'
 #     }
 
 # }
-DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-DATABASES = {
-    'default': db_from_env
-}
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+# db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config(conn_max_age=600)
 
-if 'ENGINE' in DATABASES['default'] and 'postgres' in DATABASES['default']['ENGINE']:
-    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+#if 'ENGINE' in DATABASES['default'] and 'postgres' in DATABASES['default']['ENGINE']:
+#    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 
 # django_heroku.settings(locals())
 # Password validation
