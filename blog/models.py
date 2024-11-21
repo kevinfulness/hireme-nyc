2601399a9b8607402d1514f7324f3da3d1696f7f
Kevin Plunkett <kaplunkett@gmail.com>
2024-11-21 15:58:41 -0500

    rearranged meta

--- blog/models.py
+++ blog/models.py
@@ -6,14 +6,14 @@ from django.utils.text import slugify
 class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
-    body = RichTextUploadingField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     year = models.CharField(max_length=4, null=True)
     company = models.CharField(max_length=255, null=True)
     logo = models.CharField(max_length=255, null=True)
     role = models.CharField(max_length=255, null=True)
     surface = models.CharField(max_length=255, null=True)
-    link = models.CharField(max_length=255, null=True)
+    body = RichTextUploadingField(blank=True, null=True)
+    position = models.CharField(max_length=3, null=True)
 
     def __str__(self):
         return self.title
